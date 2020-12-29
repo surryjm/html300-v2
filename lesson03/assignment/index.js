73f234081721ecf1202c59ecf1e76ec225a6b1e4
Surry <50641921+surryjm@users.noreply.github.com>
2020-12-29 10:44:37 -0800

    Edited comments on index.js

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -3,19 +3,20 @@ $(function() {
   // JSON from data.json file
   const employeesJSON = '[{"name":"Paolo Maldini","jobTitle":"Front End Developer","company":"Web Weavers","experience":"3 years","school":"University of Washington","major":"Marketing","email":"paolo@example.com","linkedInUrl":"paolo.linkedinprofile.com","codeLanguages":["HTML","CSS","JavaScript","Node","Express"]},{"name":"Barbara Bonansea","jobTitle":"Software Engineer","company":"Excellence in the Cloud","experience":"12 years","school":"University of Southern California","major":"Computer Science","email":"barbara@example.com","linkedInUrl":"barbara.linkedinprofile.com","codeLanguages":["HTML","JavaScript","C","Go"]},{"name":"Javier Hernandez","jobTitle":"User Experience Engineer","company":"Web Sites and More","experience":"5 years","school":"Seattle University","major":"Performing Arts","email":"javier@example.com","linkedInUrl":"javier.linkedinprofile.com","codeLanguages":["HTML","CSS"]},{"name":"Maribel Dominguez","jobTitle":"Front End Engineer","company":"Bits and Bytes","experience":"6 years","school":"University of Washington","major":"Mechanical Engineering","email":"maribel@example.com","linkedInUrl":"maribel.linkedinprofile.com","codeLanguages":["HTML","CSS","JavaScript","React","Vue","Redux"]}]';
   
-  // Parse the JSON and put into "employees" variable
+  // Parse the JSON
   const employees = JSON.parse(employeesJSON);
   
-  // Find the "template-hook" class in index.html to insert the data/html
+  // Find the first instance of "template-hook" class in index.html 
+  // to insert the data/html
   const container = document.querySelector('.template-hook');
 
-  // Use forEach method to apply the function per element
+  // Use forEach method to apply the function per element in the JSON array
   employees.forEach(function(el) {
     
-    // Create the "div container" to contain each object in the JSON
+    // Create the "div container" to contain each employee card
     const divContainer = document.createElement('div');
     
-    // Add the "container" class to each "div container"
+    // Apply the "container" class to the "div container"
     divContainer.classList.add('container');
     
     // Use "innerHTML" to set the html content for each "div container"
